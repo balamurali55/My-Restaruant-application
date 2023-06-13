eff9e2b42f87c1b65f3a4b9f3bb2c7ddbe37acb6
balamurali55 <balamuralismart5@gmail.com>
2023-06-13 10:23:09 +0530

    new

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class NavbarComponent extends Component {
   render() {
@@ -7,12 +7,12 @@ class NavbarComponent extends Component {
         <div className="header">
           <h5>My Restaurant</h5>
           <div className="header2">
+            <h5 style={{ padding: "0 60px" }}>Contact</h5>
             <h5>SignUp</h5>
             <h5 style={{ padding: "0 60px" }}>Login</h5>
           </div>
         </div>
         {/*  */}
-       
       </>
     );
   }
